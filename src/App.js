0afa808ee89355638f2aad7a9aca5e43136827ff
richard boucher <rb.wbscoding@gmail.com>
2020-07-30 11:16:49 +0200

    work to do

--- src/App.js
+++ src/App.js
@@ -4,9 +4,52 @@ import PictureSource from "./components/picturesource/PictureSource"
 
 function App() {
   return (
-    
+
     <PictureSource/>
+
+    // Dirk
+    // /header - trump memes
+    //tabs
+    //generator
+
+    <header></header>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+
+
+
+    // Richy Rich
+    // selected image
+    // bottom image
+    // crop images x 4
+
+
+
+    
+    // Mich
+    //Textinput form
+    // top-field
+    // bottom-field
+    // generated button
+    
+    //footer
+
+    
+
+
+
+
   );
 }
 
+
+
+
+
 export default App;
+
+
+    //2nd iteration: Randomize button
