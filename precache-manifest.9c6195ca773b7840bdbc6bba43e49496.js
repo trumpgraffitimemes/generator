b46139ac548045e2a2e999dcb8cc5a987ed030fb
Dirk Bosman <dirkjobosman@gmail.com>
2020-08-12 11:25:45 +0200

    Updates

---
+++ precache-manifest.9c6195ca773b7840bdbc6bba43e49496.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8b3bab896a8e4c9368243cf2febfa9",
+    "url": "/rb-wbscoding/meme/index.html"
+  },
+  {
+    "revision": "9e951d2e4efff2f6c4c6",
+    "url": "/rb-wbscoding/meme/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "3c2c011df4a376ccc0a8",
+    "url": "/rb-wbscoding/meme/static/css/main.904828cd.chunk.css"
+  },
+  {
+    "revision": "9e951d2e4efff2f6c4c6",
+    "url": "/rb-wbscoding/meme/static/js/2.ca774f14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rb-wbscoding/meme/static/js/2.ca774f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2c011df4a376ccc0a8",
+    "url": "/rb-wbscoding/meme/static/js/main.88a46b26.chunk.js"
+  },
+  {
+    "revision": "329784cd547124750584",
+    "url": "/rb-wbscoding/meme/static/js/runtime-main.23670b51.js"
+  },
+  {
+    "revision": "53290205673345c24884f0eac68f3b74",
+    "url": "/rb-wbscoding/meme/static/media/PatuaOne-Regular.53290205.ttf"
+  },
+  {
+    "revision": "ab647fc5e0b958dd23287310d6e6b4c4",
+    "url": "/rb-wbscoding/meme/static/media/Trump.ab647fc5.cur"
+  },
+  {
+    "revision": "c9c41fdc129c5104564532356b650786",
+    "url": "/rb-wbscoding/meme/static/media/UrbanHeroes.c9c41fdc.ttf"
+  },
+  {
+    "revision": "352100475649b296a646c84566099084",
+    "url": "/rb-wbscoding/meme/static/media/trump-lightning.35210047.gif"
+  },
+  {
+    "revision": "5bec3578fa18408e92079be09111bacf",
+    "url": "/rb-wbscoding/meme/static/media/usa.5bec3578.jpg"
+  }
+]);
