f70a5d29690805ca711824d050b83e8f04be8d8f
MrKnb <mrknabe2@gmail.com>
2020-08-13 10:49:02 +0200

    Updates

---
+++ precache-manifest.35a62e0b16aea9cb291932443ea7e589.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9c743b411c4730f5cf90f49bca5a4d",
+    "url": "/generator/index.html"
+  },
+  {
+    "revision": "9e951d2e4efff2f6c4c6",
+    "url": "/generator/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "725b6d594efe57602c4c",
+    "url": "/generator/static/css/main.3201a721.chunk.css"
+  },
+  {
+    "revision": "9e951d2e4efff2f6c4c6",
+    "url": "/generator/static/js/2.ca774f14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/generator/static/js/2.ca774f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725b6d594efe57602c4c",
+    "url": "/generator/static/js/main.0f337c03.chunk.js"
+  },
+  {
+    "revision": "12aa53500af8789de9fd",
+    "url": "/generator/static/js/runtime-main.b4fc65ac.js"
+  },
+  {
+    "revision": "53290205673345c24884f0eac68f3b74",
+    "url": "/generator/static/media/PatuaOne-Regular.53290205.ttf"
+  },
+  {
+    "revision": "ab647fc5e0b958dd23287310d6e6b4c4",
+    "url": "/generator/static/media/Trump.ab647fc5.cur"
+  },
+  {
+    "revision": "c9c41fdc129c5104564532356b650786",
+    "url": "/generator/static/media/UrbanHeroes.c9c41fdc.ttf"
+  },
+  {
+    "revision": "352100475649b296a646c84566099084",
+    "url": "/generator/static/media/trump-lightning.35210047.gif"
+  },
+  {
+    "revision": "5bec3578fa18408e92079be09111bacf",
+    "url": "/generator/static/media/usa.5bec3578.jpg"
+  }
+]);
